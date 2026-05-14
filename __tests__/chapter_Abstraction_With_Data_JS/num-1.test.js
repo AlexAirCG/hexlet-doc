efb80b05245c4137fd6c2d504c6b714527b720b0
AlexAirCG <alexairggg@yandex.ru>
2026-05-14 17:53:18 +0300

    17:53

---
+++ __tests__/chapter_Abstraction_With_Data_JS/num-1.test.js
@@ -0,0 +1,13 @@
+import calculateDistance from '../../src/chapter_Abstraction_With_Data_JS/num-1.js'
+
+test('calculeteDistance', () => {
+  const point1 = [0, 0]
+  const point2 = [3, 4]
+  expect(calculateDistance(point1, point2)).toBe(5)
+})
+
+/*
+point1 = [0, 0]
+point2 = [3, 4]
+calculateDistance(point1, point2) // 5
+// */
